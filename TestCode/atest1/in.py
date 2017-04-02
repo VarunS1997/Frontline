675deb1a7cab1f7c1d6fa580f4885162285a9d72
Varun Singh <varun.1997.singh@gmail.com>
2017-04-02 09:13:55 -0700

    final tests

--- TestCode/atest1/in.py
+++ TestCode/atest1/in.py
@@ -1,4 +1,4 @@
-
+from time import sleep
 def factorial(n):
 	if n == 1:
 		return n
@@ -7,10 +7,11 @@ def factorial(n):
 
 def main():
 	a = []
-	for i in range(100):
-		c = factorial(500)
-		a.append(c-i)
-	print(a)
+	for i in range(20):
+		c = factorial(600)
+		sleep(0.5) # in place of difficult calculation
+		a.append(c-factorial(i+500))
+		print(i)
 	return a
 
 if __name__ == '__main__':
